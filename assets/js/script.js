9b7267cf512a93e31a74d7a475af363e21a3c7c1
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-07-18 11:43:27 -0700

    fixed modal background not displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const jokeModal = document.querySelector('#joke-modal');
 const saveJokeBtn = document.querySelector("#save-joke-button")
 const savedJokes = document.querySelector("#saved-jokes")
 let jokeArr = JSON.parse(localStorage.getItem('jokeArr'))
-if(jokeArr == null){
+if (jokeArr == null) {
     jokeArr = []
     localStorage.setItem("jokeArr", JSON.stringify(jokeArr));
 }
@@ -15,57 +15,73 @@ function loadDadJoke() {
     openModal();
     let tempJoke = '';
 
-    fetch("https://icanhazdadjoke.com/",
-        { headers: { 
-            'Accept': 'application/json'
-        }})
+    fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
         .then(response => response.json())
         .then(data => {
-            console.log (jokeData)
+            console.log(jokeData)
             jokeData.id = data.id
             jokeData.joke = data.joke
-            console.log (jokeData)
+            console.log(jokeData)
             currentJoke.innerText = jokeData.joke
             saveJokeBtn.disabled = false
         });
-}
+};
 
 function loadChuckJoke() {
     currentJoke.innerText = '';
     openModal();
-    let tempJoke ='';
-    fetch("https://api.chucknorris.io/jokes/random",
-        { headers: { 
-            'Accept': 'application/json'
-        }})
+    let tempJoke = '';
+    fetch("https://api.chucknorris.io/jokes/random", {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            console.log(jokeData)
             jokeData.id = data.id
             jokeData.joke = data.value
             currentJoke.innerText = jokeData.joke
             saveJokeBtn.disabled = false
         });
-}
+};
+// function loadYoJoke() {
+//     currentJoke.innerText = '';
+//     openModal();
+//     let tempJoke = '';
+
+//     fetch("https://yomomma-api.herokuapp.com/",
+//         { headers: { 
+//             'Accept': 'application/json'
+//         }})
+//         console.log(data)
+//         .then(response => response.json())
+//         .then(data => {
+//         console.log (data)
+//         jokeData.id = data.id
+//         jokeData.joke = data.joke
+//         console.log (jokeData)
+//         currentJoke.innerText = jokeData.joke
+//         saveJokeBtn.disabled = false
+//         });
+// };
 // saves joke to local storage
 var saveJoke = function () {
     jokeArr.push(jokeData);
     localStorage.setItem("jokeArr", JSON.stringify(jokeArr));
     closeModal();
     loadSavedJokes()
-
-
-
-
-
-
 }
 
- var loadSavedJokes = function () {
+var loadSavedJokes = function () {
     //openModal()
-    
+
     const cards = jokeArr.map(j => (
-    `<div class="card">
+        `<div class="card">
             <div class="card-content">
               <div class="content" >
               ${j.joke}
@@ -74,21 +90,25 @@ var saveJoke = function () {
             <button  class= "button" onclick="deleteJoke('${j.id}')">🗑</button>
           </div>`
     )).join('')
-   // const listItems = jokeArr.map(j => ("<li>"+ j.joke + "</li>")).join("")
+    // const listItems = jokeArr.map(j => ("<li>"+ j.joke + "</li>")).join("")
     //const list = "<ul>" + listItems + "</ul>"
-   // currentJoke.innerHTML = list
+    // currentJoke.innerHTML = list
     //console.log(list)
     savedJokes.innerHTML = cards
 }
+
 loadSavedJokes();
+
 function closeModal() {
     jokeData = {};
-    jokeModal.classList.remove('is-active')   
+    jokeModal.classList.remove('is-active')
 }
+
 function openModal() {
     jokeModal.classList.add('is-active')
-     saveJokeBtn.disabled = true
+    saveJokeBtn.disabled = true
 }
+
 function deleteJoke(id) {
     jokeArr = jokeArr.filter(j => j.id != id)
     localStorage.setItem("jokeArr", JSON.stringify(jokeArr))
